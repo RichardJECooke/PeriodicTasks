cf281540abe46deb4725e8cfc3146d7113b70237
RichardJECooke <RichardJECooke@pm.me>
2025-06-27 20:04:24 +0200

    add task button adds new task

--- resources/js/taskHelper.mjs
+++ resources/js/taskHelper.mjs
@@ -2,9 +2,8 @@ import {v4 as _uuid} from 'uuid';
 import * as _constants from './constants.mjs';
 import * as _store from './store.mjs';
 
-
 export async function addTask() {
-    const task = {'id': _uuid(), 'name': '', 'days': 1, dependsOnLastCompletion: true};
+    const task = {'id': _uuid(), 'name': 'newtask', 'days': 1, dependsOnLastCompletion: true};
     console.log(task);
     _store.tasks.tasks.push(task);
 }
--- resources/js/ui.mjs
+++ resources/js/ui.mjs
@@ -1,14 +1,11 @@
-import {v4 as _uuid} from 'uuid';
+import { v4 as _uuid } from 'uuid';
 import * as _constants from './constants.mjs';
 import * as _taskHelper from './taskHelper.mjs';
 import * as _store from './store.mjs';
 
 export async function addAddTaskButton() {
-    const button = document.createElement('button');
-    button.textContent = 'Add task';
-    button.id = 'addTaskButton';
+    const button = document.getElementById('addTaskButton');
     button.onclick = addTask;
-    document.getElementById('app').appendChild(button);
 }
 
 export async function chooseSaveFileLocation() {
@@ -32,13 +29,17 @@ export async function chooseOpenFileLocation() {
     catch (e) { console.dir(e); }
 }
 
-async function addTask() {
+export async function addTask() {
     _taskHelper.addTask();
     await refreshTasks();
 }
 
 async function refreshTasks() {
-    for (let task of _store.tasks.tasks){
-
+    const tasksUi = document.getElementById('tasks');
+    tasksUi.innerHTML = ''; 
+    for (let task of _store.tasks.tasks) {
+        const li = document.createElement('li');
+        li.textContent = task.name;
+        tasksUi.appendChild(li);
     }
 }
