9e7629468bc6ad82c86f4dd6437d888a0db83d62
RichardJECooke <RichardJECooke@pm.me>
2025-08-11 17:49:35 +0200

    working on editing task

--- resources/js/vue/taskListItem.vue
+++ resources/js/vue/taskListItem.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
-import { defineProps } from 'vue';
 import type { task } from '../types.ts';
+import { ref, type Ref } from 'vue';
 
 const props = defineProps<{task: task}>();
+let isEditing: Ref<boolean> = ref(false);
+
+function toggleEditing() { isEditing.value = !isEditing.value; }
 </script>
 
 <template>
-<li>
-  <span>{{ props.task.name }} &nbsp;&nbsp;&nbsp;</span>
-  <span>Repeats every {{ props.task.days }} days &nbsp;&nbsp;&nbsp;</span>
-  <label>
-    Next due date depends on last completion date:
-    <input type="checkbox" :checked="props.task.dependsOnLastCompletion" disabled />
-  </label>
+<li @click="toggleEditing">
+<!-- Readonly -->  
+  <div class="readonly" v-if="!isEditing">
+    <span>{{ props.task.name }}</span>
+    · Due in 5 days
+  </div>
+<!-- Editing   -->
+  <div class="readonly" v-if="isEditing">
+    <input  type="text" v-model="props.task.name" />
+    · 
+    <span>Repeats every {{ props.task.days }} days</span>
+    · 
+    <label>
+      Next due date depends on last completion date:
+      <input type="checkbox" :checked="props.task.dependsOnLastCompletion" />
+    </label>
+  </div>
 </li>
 </template>
