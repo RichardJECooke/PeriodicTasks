08ae2b2aea8face85f9072912d1aaf9ab81cc743
RichardJECooke <RichardJECooke@pm.me>
2025-08-12 18:27:07 +0200

    got edit task to work

--- resources/js/vue/taskListItem.vue
+++ resources/js/vue/taskListItem.vue
@@ -5,26 +5,37 @@ import { ref, type Ref } from 'vue';
 const props = defineProps<{task: task}>();
 let isEditing: Ref<boolean> = ref(false);
 
-function toggleEditing() { isEditing.value = !isEditing.value; }
+function startEditing() { isEditing.value = true; }
+function stopEditing() { isEditing.value = false; }
+function allowOnlyDigits(event: Event) {
+  const input = event.target as HTMLInputElement;
+  input.value = input.value.replace(/[^0-9]/g, '');
+  props.task.days = Number(input.value) || 1;
+}
 </script>
 
 <template>
-<li @click="toggleEditing">
-<!-- Readonly -->  
-  <div class="readonly" v-if="!isEditing">
+<li>
+<!-- Readonly -->
+  <div class="readonly" v-if="!isEditing"  v-on:click="startEditing">
     <span>{{ props.task.name }}</span>
     · Due in 5 days
   </div>
 <!-- Editing   -->
-  <div class="readonly" v-if="isEditing">
+  <div class="editing" v-if="isEditing">
     <input  type="text" v-model="props.task.name" />
-    · 
-    <span>Repeats every {{ props.task.days }} days</span>
-    · 
+    <br />
+    <span>Repeats every
+      <input type="number" step="1" min="1" v-bind:value="props.task.days" v-on:input="allowOnlyDigits" />
+      days
+    </span>
+    <br />
     <label>
       Next due date depends on last completion date:
-      <input type="checkbox" :checked="props.task.dependsOnLastCompletion" />
+      <input type="checkbox" v-bind:checked="props.task.dependsOnLastCompletion" />
     </label>
+    <br />
+    <input type="button" v-on:click="stopEditing" value="Done" />
   </div>
 </li>
 </template>
