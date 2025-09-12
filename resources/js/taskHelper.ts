51810fc70f3668967f0a8a9b87aa7c8c15bf9103
RichardJECooke <RichardJECooke@pm.me>
2025-09-12 22:30:45 +0200

    save

--- resources/js/taskHelper.ts
+++ resources/js/taskHelper.ts
@@ -10,11 +10,31 @@ export async function addTask(): Promise<void> {
 
 export function getNumDaysUntilDue(task : types.task): number {
   if (task.datesDone.length == 0) return 0;
-  const lastDoneDate = task.datesDone.sort((a, b) => b.getTime() - a.getTime())[0];
-  const today = new Date();
+  task.datesDone.map((d) => console.log(d));
+  const lastDoneDate = task.datesDone.toSorted((a, b) => b.getTime() - a.getTime())[0];
   lastDoneDate.setHours(0, 0, 0, 0);
-  today.setHours(0, 0, 0, 0);
   const msPerDay = 1000 * 60 * 60 * 24;
-  const daysSinceLastDone = Math.floor((today.getTime() - lastDoneDate.getTime()) / msPerDay);
+  const daysSinceLastDone = Math.floor((getToday().getTime() - lastDoneDate.getTime()) / msPerDay);
   return task.days - daysSinceLastDone;
+}
+
+export function getToday(): Date {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  return today;
+}
+
+export function addDoneToday(task: types.task): void {
+  task.datesDone.push(getToday());
+}
+
+export function isSameDay (first: Date, second: Date): boolean {
+  return (first.getFullYear() === second.getFullYear() &&
+  first.getMonth() === second.getMonth() &&
+  first.getDate() === second.getDate());
+}
+
+export function removeDoneToday(task: types.task): void {
+  const today = getToday();
+  task.datesDone = task.datesDone.filter(date => !isSameDay(date, today));
 }
--- resources/js/vue/taskListItem.vue
+++ resources/js/vue/taskListItem.vue
@@ -8,6 +8,16 @@ const props = defineProps<{task: types.task}>();
 let isEditing: vue.Ref<boolean> = vue.ref(false);
 const getNumDaysUntilDue = _taskHelper.getNumDaysUntilDue; // entire file imports are not available in the template
 
+function handleMarkDone(event: Event) {
+  const target = event.target as HTMLInputElement;
+  if (target.checked) _taskHelper.addDoneToday(props.task);
+  else _taskHelper.removeDoneToday(props.task);
+}
+const isDoneToday = vue.computed(() => {
+  const today = _taskHelper.getToday();
+  return props.task.datesDone.some((date) => _taskHelper.isSameDay(new Date(date), today));
+});
+
 function startEditing() { isEditing.value = true; }
 function stopEditing() { isEditing.value = false; }
 </script>
@@ -15,7 +25,10 @@ function stopEditing() { isEditing.value = false; }
 <template>
 <li>
 <!-- Readonly -->
-  <div class="readonly" v-if="!isEditing"  v-on:click="startEditing">
+  <div v-if="!isEditing"  >
+    <input type="checkbox" id="doneToday" v-bind:checked="isDoneToday" v-on:change="handleMarkDone"/>
+    <label for="doneToday">Done today</label>
+    &nbsp;&nbsp;
     <span>{{ props.task.name }}</span>
     ·
     Due
@@ -24,9 +37,12 @@ function stopEditing() { isEditing.value = false; }
     <span v-if="getNumDaysUntilDue(props.task) <0 && getNumDaysUntilDue(props.task) >-1">{{ getNumDaysUntilDue(props.task) }} day ago</span>
     <span v-if="getNumDaysUntilDue(props.task) >0 && getNumDaysUntilDue(props.task) <1">in {{ getNumDaysUntilDue(props.task) }} day</span>
     <span v-if="getNumDaysUntilDue(props.task) >1">in {{ getNumDaysUntilDue(props.task) }} days</span>
+    &nbsp;&nbsp;
+    <input type="button" v-on:click="startEditing" value="Edit" />
   </div>
 <!-- Editing   -->
   <TaskListItemEdit v-if="isEditing" :key="props.task.id" :task="props.task"
      :isEditing="isEditing" @stopEditingEvent="stopEditing" />
 </li>
+<br />
 </template>
