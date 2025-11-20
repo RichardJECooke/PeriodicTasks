bdc46246a0b4e7b0ef3e0a5708474f2e14214179
RichardJECooke <RichardJECooke@pm.me>
2025-11-20 12:25:43 +0200

    got ipc to work in electron

--- electron/resources/ts/main.js
+++ electron/resources/ts/main.js
@@ -1,8 +1,9 @@
 const path = require('node:path');
-const { app, BrowserWindow } = require('electron/main');
+const { app, BrowserWindow, ipcMain } = require('electron/main');
 
 app.whenReady().then(() => { // don't change to .on or start breaks https://github.com/electron/electron/pull/21972
   if (process.platform !== 'linux') app.quit();
+  ipcMain.handle('ping', () => 'pong');
   const win = new BrowserWindow({width: 800, height: 600,  webPreferences: { preload: path.join(__dirname, 'preload.js')} });
   win.loadFile('resources/html/index.html');
 })
--- electron/resources/ts/renderer.js
+++ electron/resources/ts/renderer.js
@@ -1,2 +1,9 @@
 const information = document.getElementById('info');
 information.innerText = `This app is using Chromium (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
+
+async function d(){
+const s = await window.versions.ping();
+information.innerText = s;
+}
+
+d();
