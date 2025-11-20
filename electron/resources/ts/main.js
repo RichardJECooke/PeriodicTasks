fe5cbc9f0d1eb9fde2f51a5b019350f3cf714684
RichardJECooke <RichardJECooke@pm.me>
2025-11-20 12:10:44 +0200

    moving files to folders

--- electron/resources/ts/main.js
+++ electron/resources/ts/main.js
@@ -1,9 +1,10 @@
+const path = require('node:path');
 const { app, BrowserWindow } = require('electron/main');
 
 app.whenReady().then(() => { // don't change to .on or start breaks https://github.com/electron/electron/pull/21972
   if (process.platform !== 'linux') app.quit();
-  const win = new BrowserWindow({width: 800, height: 600 });
-  win.loadFile('index.html');
+  const win = new BrowserWindow({width: 800, height: 600,  webPreferences: { preload: path.join(__dirname, 'preload.js')} });
+  win.loadFile('resources/html/index.html');
 })
 
 app.on('window-all-closed', () => { app.quit(); })
